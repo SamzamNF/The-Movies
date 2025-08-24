fcf248cadb5d489434554bb1607e371b2210cfbc
Nicolai Friis <nmfr71398@edu.ucl.dk>
2025-08-25 00:41:55 +0200

    Binded everything + changed to combobox in view for ScheduleControl Some minor bugs with overlapping comboboxes fading the UI in front of eachother that could be fixed

--- The Movies/The Movies/View/ScheduleControl.xaml.cs
+++ The Movies/The Movies/View/ScheduleControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using The_Movies.ViewModel;
 
 namespace The_Movies.View
 {
@@ -21,6 +22,8 @@ namespace The_Movies.View
         public ScheduleControl()
         {
             InitializeComponent();
+            ScheduleControlViewModel mv = new ScheduleControlViewModel();
+            DataContext = mv;
         }
     }
 }
